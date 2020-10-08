0fcffb3d84ef1b42486e3a6252ef5ce1b501362a
jenniferbasford <basford.jennifer@gmail.com>
2020-10-08 14:42:59 -0600

    created header/navbar components and images folder liked to homepage

---
+++ src/components/headernavbar/header.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Header extends Component {
+  render() {
+    return (
+      <div className="header">
+        <img src="images/Honeysuckle Logo white.jpg" />
+        <img src="images/header-background.jpg" />
+      </div>
+    );
+  }
+}
+
+export default Header;
--- src/components/home.js
+++ src/components/home.js
@@ -1,9 +1,14 @@
 import React from "react";
 
+import Header from "./headernavbar/header";
+import Navbar from "./headernavbar/navbar";
+
 export default function () {
   return (
-    <div>
+    <div className="home">
+      <Header />
       <h1>Home</h1>
+      <Navbar />
     </div>
   );
 }
