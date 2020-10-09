40f9af2dc142ac549c9f9d03a9e94cfc949d1531
jenniferbasford <basford.jennifer@gmail.com>
2020-10-09 13:02:09 -0600

    added navbar components and menu items for navbar

--- src/components/app.js
+++ src/components/app.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 
-export default class App extends Component {
+class App extends Component {
   render() {
     return (
       <div className="app">
@@ -9,3 +9,5 @@ export default class App extends Component {
     );
   }
 }
+
+export default App;
--- src/components/headernavbar/navbar.js
+++ src/components/headernavbar/navbar.js
@@ -1,8 +1,26 @@
 import React, { Component } from "react";
 
+import { MenuItems } from "./menuItems";
+
 class Navbar extends Component {
   render() {
-    return <div className="navbar"></div>;
+    return (
+      <nav className="navbarItems">
+        <h1 className="navbar-logo">Honeysuckle</h1>
+        <div className="menu-icon"></div>
+        <ul>
+          {MenuItems.map((item, index) => {
+            return (
+              <li key={index}>
+                <a className={item.cName} href={item.url}>
+                  {item.title}
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+      </nav>
+    );
   }
 }
 
