d8c7a8bd0653e77b47afbc2a2ea8d9dc57255a74
jenniferbasford <basford.jennifer@gmail.com>
2020-12-02 11:28:51 -0700

    made navbar mobile responsive with hamburger menu

--- src/components/headernavbar/navbar.js
+++ src/components/headernavbar/navbar.js
@@ -1,18 +1,42 @@
 import React, { Component } from "react";
 import { NavLink } from "react-router-dom";
+import { Menu, X } from "react-feather";
 
 class Navbar extends Component {
+  state = { clicked: false };
+
+  handleClick = () => {
+    this.setState({ clicked: !this.state.clicked });
+  };
+
   render() {
     return (
       <div className="nav-wrapper">
-        <div className="nav-links">
+        <div className="menu-icon" onClick={this.handleClick}>
+          {this.state.clicked ? <X /> : <Menu />}
+        </div>
+        <div className={this.state.clicked ? "nav-links-active" : "nav-links"}>
+          <NavLink className="navbar-link" exact to="/">
+            Home
+          </NavLink>
+          <NavLink className="navbar-link" to="/about-us">
+            About Us
+          </NavLink>
+          <NavLink className="navbar-link" to="/menu">
+            Menu
+          </NavLink>
+          <NavLink className="navbar-link" to="/contact-us">
+            Contact Us
+          </NavLink>
+        </div>
+        {/* <div className={this.state.clicked ? "nav-links-active" : "nav-links"}>
           <NavLink exact to="/">
             Home
           </NavLink>
           <NavLink to="/about-us">About Us</NavLink>
           <NavLink to="/menu">Menu</NavLink>
           <NavLink to="/contact-us">Contact Us</NavLink>
-        </div>
+        </div> */}
       </div>
     );
   }
